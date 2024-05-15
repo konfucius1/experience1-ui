d6640e1a04987ac0524d9c5d4509b0532ad45a2c
Thanakon <kimsanthanakon@gmail.com>
2024-05-15 16:27:48 +1000

    feat: set up theme and primary color saved to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
 import { MantineProvider, createTheme } from '@mantine/core';
-import { Router } from './Router';
+import { useLocalStorage } from '@mantine/hooks';
+import { Router } from './routes/Router';
 // 👇 Import the mantine-core layer CSS file;
 //    this will automatically place it in a `mantine` layer
 import '@mantine/core/styles.layer.css';
@@ -8,13 +9,21 @@ import '@mantine/core/styles.layer.css';
 // 👇 Import the mantine-datatable layer CSS file;
 //    this will automatically place it in a `mantine-datatable` layer
 import 'mantine-datatable/styles.layer.css';
-
-const theme = createTheme({
-  fontFamily: 'Montserrat, sans-serif',
-  primaryColor: 'cyan',
-});
+import { deepBlue } from './components/ColorTheme/team';
 
 export default function App() {
+  const [primaryColorStored] = useLocalStorage({
+    key: 'primary-color',
+    defaultValue: 'blue',
+  });
+
+  const theme = createTheme({
+    fontFamily: 'Montserrat, sans-serif',
+    primaryColor: primaryColorStored,
+    colors: {
+      deepBlue,
+    },
+  });
   return (
     <MantineProvider theme={theme}>
       <Router />
