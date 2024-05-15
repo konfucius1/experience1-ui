401debc29932a10c1c1b8116633d1ecf618d0f34
Thanakon <kimsanthanakon@gmail.com>
2024-05-15 16:31:20 +1000

    feat: clear home page

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,13 +1,11 @@
-import { DriversStanding } from '@/features/DriversStanding/DriversStanding';
 import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Header } from '@/components/Header/Header';
 
 export function HomePage() {
   return (
     <>
+      <Header />
       <Welcome />
-      <ColorSchemeToggle />
-      <DriversStanding />
     </>
   );
 }
